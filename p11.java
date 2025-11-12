d67e30a472e21c74e63cdc8fedc15f9ee9319091
Aadarsh0905 <aadarshtmsl2027@gmail.com>
2025-11-12 18:12:09 +0530

    Add files via upload

---
+++ p11.java
@@ -0,0 +1,25 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class p11 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Give the no. of test cases you want in the output: ");
+        int t=in.nextInt();
+        for (int i = 0; i <t; i++) {
+            System.out.print("Enter the number of rows to print: ");
+            int n=in.nextInt();
+            p11(n);
+        }
+    }
+    static void p11(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if((i+j)%2==0) System.out.print("1"+" ");
+                else System.out.print("0"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
